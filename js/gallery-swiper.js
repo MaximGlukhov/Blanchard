b0693af2358e6f6fc4bf649595336bf813a45d79
MaximGlukhov <gluxov-1990@mail.ru>
2021-05-29 16:47:52 +0500

    first

---
+++ js/gallery-swiper.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.section-gallery__swiper', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next-new',
+      prevEl: '.swiper-button-prev-new',
+    },
+    autoHeight: false,
+    
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+   
+      660: {
+        slidesPerView: 2,
+        spaceBetween: 34,
+        slidesPerGroup: 2,
+        slidesPerColumn: 2,
+      },
+      
+      1601: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        slidesPerGroup: 4,
+        slidesPerColumn: 2,
+      }
+    }
+  });
+
