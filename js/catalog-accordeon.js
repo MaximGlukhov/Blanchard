d69b4cfca04bdef8583cfbf5492a6572d7449418
MaximGlukhov <gluxov-1990@mail.ru>
2021-08-17 17:00:41 +0500

    scrollbar

--- js/catalog-accordeon.js
+++ js/catalog-accordeon.js
@@ -2,18 +2,16 @@ $(function () {
   $(".section-catalog__list-accordeon").accordion({
     collapsible: true,
     active: false,
-    animate: 100,
+    animate: 500,
     heightStyle: "content"
   });
 });
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.section-catalog__item-accordeon__btn').addEventListener('click', function() {
-    $( function() {
-      $('.section-catalog__list-accordeon').accordion("refresh");
-    });
-  })
-})
+
+
+
+
+
 
 
 
