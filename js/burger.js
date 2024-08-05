4aeb3c235add48be68e614b283a36e5f94b7758c
MaksimGlukhov <gluxov-1990@mail.ru>
2024-08-05 18:09:14 +0500

    new

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,13 @@
-window.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#burger').addEventListener('click', function() {
-        document.querySelector('#menu').classList.toggle('is-active')
-    })
-  })
+window.addEventListener( 'DOMContentLoaded', function () {
+  document.querySelector( '#burger' ).addEventListener( 'click', function () {
+    document.querySelector( '#menu' ).classList.toggle( 'is-active' )
+  } )
+} )
 
 
 
-window.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#burger-close').addEventListener('click', function() {
-        document.querySelector('#menu').classList.toggle('is-active')
-    })
-  })
-  
+window.addEventListener( 'DOMContentLoaded', function () {
+  document.querySelector( '#burger-close' ).addEventListener( 'click', function () {
+    document.querySelector( '#menu' ).classList.toggle( 'is-active' )
+  } )
+} )
